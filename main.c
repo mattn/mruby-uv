3d3de3137110534f000ad5819e52a9a83b86a57e
mattn <mattn.jp@gmail.com>
2012-04-20 22:53:51 +0900

    simplify.

--- main.c
+++ main.c
@@ -11,23 +11,19 @@ main()
   int n;
   mrb_state* mrb;
   struct mrb_parser_state* st;
-
-  mrb = mrb_open();
-
-  mrb_uv_init(mrb);
-
-  {
-    char* code =
+  char* code =
 "require 'UV'             \n"
 "t = UV::Timer.new()      \n"
 "t.start(1000, 1000) {|x| \n"
 "  p x                    \n"
 "}                        \n"
 "p UV.run()               \n";
-    st = mrb_parse_string(mrb, code);
-    n = mrb_generate_code(mrb, st->tree);
-    mrb_pool_close(st->pool);
-    mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value());
-  }
+
+  mrb = mrb_open();
+  mrb_uv_init(mrb);
+  st = mrb_parse_string(mrb, code);
+  n = mrb_generate_code(mrb, st->tree);
+  mrb_pool_close(st->pool);
+  mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value());
 }
 
