b652e054f14fb4a3c5d9b4baf1964baf30b55d6b
mattn <mattn.jp@gmail.com>
2012-04-22 08:20:36 +0900

    add example for loop. currently broken.

---
+++ loop.c
@@ -0,0 +1,38 @@
+#include <mruby.h>
+#include <mruby/proc.h>
+#include <mruby/data.h>
+#include <compile.h>
+#include <variable.h>
+#include <mrb_uv.h>
+
+#define _(...) #__VA_ARGS__ "\n"
+
+int
+main()
+{
+  int n;
+  mrb_state* mrb;
+  struct mrb_parser_state* st;
+  char* code =
+_( require 'UV'                              )
+_( l = UV::Loop.new()                        )
+_( t = UV::Timer.new(l)                      )
+_( i = 3                                     )
+_( t.start(1000, 1000) {|x|                  )
+_(   p i                                     )
+_(   i -= 1                                  )
+_(   if i < 0                                )
+_(     t.close()                             )
+_(     l.delete()                            )
+_(   end                                     )
+_( }                                         )
+_( l.run()                                   );
+
+  mrb = mrb_open();
+  mrb_uv_init(mrb);
+  st = mrb_parse_string(mrb, code);
+  n = mrb_generate_code(mrb, st->tree);
+  mrb_pool_close(st->pool);
+  mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value());
+  return 0;
+}
