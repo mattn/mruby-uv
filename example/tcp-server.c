4f3c7a2731e9968dab79184d80f37a041717b059
mattn <mattn.jp@gmail.com>
2012-04-24 18:58:16 +0900

    add example for tcp server.

---
+++ example/tcp-server.c
@@ -0,0 +1,40 @@
+#include <mruby.h>
+#include <mruby/proc.h>
+#include <mruby/data.h>
+#include <compile.h>
+#include <variable.h>
+#include <mrb_uv.h>
+
+#define _(...) #__VA_ARGS__ "\n"
+
+int
+main()
+{
+  int n;
+  mrb_state* mrb;
+  struct mrb_parser_state* st;
+  char* code =
+ _(
+)_( require 'UV'
+)_( t = UV::TCP.new()
+)_( t.bind(UV.ip4_addr('127.0.0.1', 8888))
+)_( t.listen(5) {|x|
+)_(   return if x != 0
+)_(   c = t.accept()
+)_(   puts "connected"
+)_(   c.write('helloworld') {|x|
+)_(     puts "sent data"
+)_(     c.close()
+)_(   }
+)_( }
+)_( UV.run()
+);
+
+  mrb = mrb_open();
+  mrb_uv_init(mrb);
+  st = mrb_parse_string(mrb, code);
+  n = mrb_generate_code(mrb, st->tree);
+  mrb_pool_close(st->pool);
+  mrb_run(mrb, mrb_proc_new(mrb, mrb->irep[n]), mrb_nil_value());
+  return 0;
+}
