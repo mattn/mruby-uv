081bcfa1c2dfee6a710178abae4ec9fdfb08340c
mattn <mattn.jp@gmail.com>
2012-12-14 13:28:23 +0900

    filesystem operations with callbacks.

--- example/fs.rb
+++ example/fs.rb
@@ -1,16 +1,18 @@
 #!mruby
-#f = UV::FS::open("foo.txt", UV::FS::O_CREAT|UV::FS::O_WRONLY, UV::FS::S_IWRITE)
-#f.write("fooo")
-#f.close()
 
-f = UV::FS::open("foo.txt", UV::FS::O_RDONLY) {
-  puts f.read(3)
-  puts f.read()
-  f.close()
-}
-
-#UV::FS::unlink("foo.txt")
-#UV::FS::mkdir("foo-bar")
-#UV::FS::rmdir("foo-bar")
-
+UV::FS::mkdir("foo-bar") do
+  f = UV::FS::open("foo-bar/foo.txt", UV::FS::O_CREAT|UV::FS::O_WRONLY, UV::FS::S_IWRITE)
+  f.write("fooo")
+  f.close() do
+    f = UV::FS::open("foo-bar/foo.txt", UV::FS::O_RDONLY) do
+      puts f.read(3)
+      puts f.read()
+      f.close() do
+        UV::FS::unlink("foo-bar/foo.txt") do
+          UV::FS::rmdir("foo-bar")
+        end
+      end
+    end
+  end
+end
 UV::run()
