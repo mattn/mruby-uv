e7d19cc375287082d01ec7350cc412d8a0ad5d67
mattn <mattn.jp@gmail.com>
2012-12-14 09:32:25 +0900

    remove O_TEXT.

--- example/fs.rb
+++ example/fs.rb
@@ -3,7 +3,7 @@ f = UV::FS::open("foo.txt", UV::FS::O_CREAT|UV::FS::O_WRONLY, UV::FS::S_IWRITE)
 f.write("fooo")
 f.close()
 
-f = UV::FS::open("foo.txt", UV::FS::O_RDONLY|UV::FS::O_TEXT, UV::FS::S_IREAD)
+f = UV::FS::open("foo.txt", UV::FS::O_RDONLY, UV::FS::S_IREAD)
 puts f.read(3)
 puts f.read()
 f.close()
