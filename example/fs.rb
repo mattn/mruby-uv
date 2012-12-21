f44a015de3ccc21fa5190f50e9d284190ab5b603
mattn <mattn.jp@gmail.com>
2012-12-21 21:25:42 +0900

    fix example.

--- example/fs.rb
+++ example/fs.rb
@@ -4,7 +4,7 @@ UV::FS::mkdir("foo-bar") do
   f = UV::FS::open("foo-bar/foo.txt", UV::FS::O_CREAT|UV::FS::O_RDWR, UV::FS::S_IREAD)
   f.write("fooo") do
     f.close() do
-      f = UV::FS::open("foo.txt", UV::FS::O_RDONLY, UV::FS::S_IREAD) do
+      f = UV::FS::open("foo-bar/foo.txt", UV::FS::O_RDONLY, UV::FS::S_IREAD) do
         puts f.read(3)
         puts f.read()
         f.close() do
