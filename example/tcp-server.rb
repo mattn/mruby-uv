702d8ab54b1c22e7855ff45309ad063ff130fcfe
mattn <mattn.jp@gmail.com>
2013-01-17 22:36:09 +0900

    handle SIGPIPE.

--- example/tcp-server.rb
+++ example/tcp-server.rb
@@ -1,26 +1,30 @@
 #!mruby
 
-s = UV::TCP.new()
+t = UV::Timer.new
+UV::Signal.new.start(UV::Signal::SIGPIPE) do
+  puts "connection closed"
+  t.stop
+end
+
+s = UV::TCP.new
 s.bind(UV::ip4_addr('127.0.0.1', 8888))
 s.listen(5) {|x|
   return if x != 0
-  c = s.accept()
+  c = s.accept
   puts "connected"
   c.write "helloworld\r\n"
-  t = UV::Timer.new()
   t.start(1000, 1000) {|x|
     puts "helloworld\n"
     begin
       c.write "helloworld\r\n"
     rescue RuntimeError
-      c.close()
+		puts "foo"
+      c.close
       c = nil
-      t.stop()
+      t.stop
       t = nil
     end
   }
 }
-
-UV::Signal.new.start(UV::Signal::SIGPIPE) {}
 
 UV::run()
