a6145c64f5ac104e41b1299ad22b27bd73cb888d
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-07-12 14:32:04 -0500

    Add examples for TCP IPv6

---
+++ example/tcp-server-ipv6.rb
@@ -0,0 +1,33 @@
+#!mruby
+
+t = UV::Timer.new
+
+if UV::Signal.const_defined?(:SIGPIPE)
+  UV::Signal.new.start(UV::Signal::SIGPIPE) do
+    puts "connection closed"
+    t.stop
+  end
+end
+
+s = UV::TCP.new
+s.bind6(UV::ip6_addr('::1', 8888))
+s.listen(5) {|x|
+  return if x != 0
+  c = s.accept
+  puts "connected"
+  c.write "helloworld\r\n"
+  t.start(1000, 1000) {|x|
+    puts "helloworld\n"
+    begin
+      c.write "helloworld\r\n"
+    rescue RuntimeError
+      puts "disconnected"
+      c.close
+      c = nil
+      t.stop
+      t = nil
+    end
+  }
+}
+
+UV::run()
