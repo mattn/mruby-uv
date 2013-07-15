9eaf110dd843ff9f0dfc40f43bf220aaf0a3c54c
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-07-15 10:34:35 -0500

    Add UV::TCP#getpeername

--- example/tcp-server.rb
+++ example/tcp-server.rb
@@ -14,7 +14,7 @@ s.bind(UV::ip4_addr('127.0.0.1', 8888))
 s.listen(5) {|x|
   return if x != 0
   c = s.accept
-  puts "connected"
+  puts "connected (peer: #{c.getpeername})"
   c.write "helloworld\r\n"
   t.start(1000, 1000) {|x|
     puts "helloworld\n"
